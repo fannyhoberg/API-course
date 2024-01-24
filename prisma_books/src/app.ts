3cde1bf38a817ba30ffee040c323a4b57d1f2718
Fanny Hoberg <fannyh.92@hotmail.com>
2024-01-24 09:24:47 +0100

    added index.ts and moved routes

--- prisma_books/src/app.ts
+++ prisma_books/src/app.ts
@@ -1,35 +1,11 @@
 import express from "express";
 import morgan from "morgan";
-import authorRoutes from "./routes/authors";
-import bookRoutes from "./routes/books";
-import publisherRoutes from "./routes/publishers";
+import routes from "./routes";
 
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 
-// use routes
-app.use(authorRoutes);
-app.use(bookRoutes);
-app.use(publisherRoutes);
-
-/**
- * GET /
- */
-app.get("/", (req, res) => {
-  res.send({
-    message: "I AM API, BEEP BOOP",
-  });
-});
-
-/**
- * Catch-all route handler
- */
-app.use((req, res) => {
-  // Respond with 404 and a message in JSON-format
-  res.status(404).send({
-    message: "Not Found",
-  });
-});
+app.use(routes);
 
 export default app;
