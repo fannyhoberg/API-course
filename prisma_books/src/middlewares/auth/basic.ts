ea2f6759961497b56009164326db0603d2c3ab4b
Fanny Hoberg <fannyh.92@hotmail.com>
2024-01-29 16:01:10 +0100

    added basic.ts

--- prisma_books/src/middlewares/auth/basic.ts
+++ prisma_books/src/middlewares/auth/basic.ts
@@ -8,6 +8,46 @@ import { Request, Response, NextFunction } from "express";
 const debug = Debug("prisma-books:basic");
 
 export const basic = (req: Request, res: Response, next: NextFunction) => {
-  debug("Hello from auth/basic");
+  debug("Hello from auth/basic", req.headers);
+
+  // 1. Make sure Authorization header exists, otherwise bail 🛑
+
+  if (!req.headers.authorization) {
+    debug("Authorization header missing"!);
+    return res
+      .status(401)
+      .send({ status: "fail", message: "Authorization required" });
+  }
+  // 2. Split Authorization header on ` `
+  // "Basic am5AdGhlaGl2ZXJlc2lzdGFuY2UuY29tOmFiYzEyMw=="
+  // =>
+  // [0] => "Basic"
+  // [1] => "am5AdGhlaGl2ZXJlc2lzdGFuY2UuY29tOmFiYzEyMw=="
+
+  debug("Authorization header: &o", req.headers.authorization);
+  const [authSchema, base64Payload] = req.headers.authorization.split(" ");
+
+  // 3. Check that Authorization scheme is "Basic", otherwise bail 🛑
+  if (authSchema.toLowerCase() !== "basic") {
+    debug("Authorization schema isn't basic"!);
+    return res
+      .status(401)
+      .send({ status: "fail", message: "Authorization schema isn't basic" });
+  }
+
+  // 4. Decode credentials from base64 => ascii
+  const decodedPayload = Buffer.from(base64Payload, "base64").toString("ascii");
+
+  // 5. Split credentials on `:`
+  const [email, password] = decodedPayload.split(":");
+
+  // 6. Get user from database, otherwise bail 🛑
+
+  // 7. Verify hash against credentials, otherwise bail 🛑
+
+  // 8. Attach user to request
+
+  // 9. Profit 🤑
+
   next();
 };
