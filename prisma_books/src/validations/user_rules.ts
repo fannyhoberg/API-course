1eaa7488d12c0228923699edc7091bd29b4c39c9
Fanny Hoberg <fannyh.92@hotmail.com>
2024-01-29 11:03:02 +0100

    customized validation for user email to check if it already existsr

--- prisma_books/src/validations/user_rules.ts
+++ prisma_books/src/validations/user_rules.ts
@@ -2,6 +2,7 @@
  * Validation rules for user resource
  */
 import { body } from "express-validator";
+import prisma from "../prisma";
 
 export const createUserRules = [
   body("name")
@@ -11,7 +12,25 @@ export const createUserRules = [
     .trim()
     .isLength({ min: 3 })
     .withMessage("name has to be at least 3 chars long"),
-  body("email").trim().isEmail().withMessage("email has to be a valid email"),
+  body("email")
+    .trim()
+    .isEmail()
+    .withMessage("email has to be a valid email")
+    .bail()
+    .custom(async (value) => {
+      // check if a user with that email already exists
+      const user = await prisma.user.findUnique({
+        where: {
+          email: value,
+        },
+      });
+
+      if (user) {
+        // user already exists, reject promise
+        // return Promise.reject("Email already exists");
+        throw new Error("Email already exists in database");
+      }
+    }),
   body("password")
     .isString()
     .withMessage("password has to be a string")
