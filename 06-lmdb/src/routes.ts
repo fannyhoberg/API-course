12c73e37408beba659b2ba629b784a648387aa1e
Fanny Hoberg <fannyh.92@hotmail.com>
2024-02-12 12:03:44 +0100

    added route for movies, can now get all movies

--- 06-lmdb/src/routes.ts
+++ 06-lmdb/src/routes.ts
@@ -1,23 +1,29 @@
 import express from "express";
+import movieRouter from "./resources/movie/movie.router";
 const router = express.Router();
 
 /**
  * GET /
  */
 router.get("/", (req, res) => {
-	res.send({
-		message: "I AM MOVIE-DB-API, GIFES POPCORN PLZ 🍿",
-	});
+  res.send({
+    message: "I AM MOVIE-DB-API, GIFES POPCORN PLZ 🍿",
+  });
 });
+
+/**
+ * /movies
+ */
+router.use("/movies", movieRouter);
 
 /**
  * Catch-all route handler
  */
 router.use((req, res) => {
-	// Respond with 404 and a message in JSON-format
-	res.status(404).send({
-		message: `Route ${req.method} ${req.path} does not exist`,
-	});
+  // Respond with 404 and a message in JSON-format
+  res.status(404).send({
+    message: `Route ${req.method} ${req.path} does not exist`,
+  });
 });
 
 export default router;
