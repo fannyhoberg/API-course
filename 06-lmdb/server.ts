c6845053a1789015230dd93ebd7f4446c7d51121
Fanny Hoberg <fannyh.92@hotmail.com>
2024-02-12 11:12:19 +0100

    connected to mongodb atlas

--- 06-lmdb/server.ts
+++ 06-lmdb/server.ts
@@ -1,6 +1,7 @@
-import app from "./src/app";
-import http from "http";
 import * as dotenv from "dotenv";
+import http from "http";
+import app from "./src/app";
+import { connect } from "./src/database";
 
 // Initialize dotenv so it reads our `.env`-file
 dotenv.config();
@@ -14,35 +15,45 @@ const PORT = Number(process.env.PORT) || 3000;
 const server = http.createServer(app);
 
 /**
- * Listen on provided port, on all network interfaces.
+ * Connect to database, and then listen on provided port, on all network interfaces.
  */
-server.listen(PORT);
+connect()
+  .then(() => {
+    // Start responding to incoming requests
+    server.listen(PORT);
+  })
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
 
 /**
  * Event listener for HTTP server "error" event.
  */
 server.on("error", (err: NodeJS.ErrnoException) => {
-	if (err.syscall !== "listen") {
-		throw err;
-	}
+  if (err.syscall !== "listen") {
+    throw err;
+  }
 
-	switch (err.code) {
-		case "EACCES":
-			console.error(`🦸🏻 Port ${PORT} requires elevated privileges`);
-			process.exit(1);
-			break;
-		case "EADDRINUSE":
-			console.error(`🛑 Port ${PORT} is already in use in another of your fifty thousand terminals 😜`);
-			process.exit(1);
-			break;
-		default:
-			throw err;
-	}
+  switch (err.code) {
+    case "EACCES":
+      console.error(`🦸🏻 Port ${PORT} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(
+        `🛑 Port ${PORT} is already in use in another of your fifty thousand terminals 😜`
+      );
+      process.exit(1);
+      break;
+    default:
+      throw err;
+  }
 });
 
 /**
  * Event listener for HTTP server "listening" event.
  */
 server.on("listening", () => {
-	console.log(`🍿 Yay, server started on http://localhost:${PORT}`);
+  console.log(`🍿 Yay, server started on http://localhost:${PORT}`);
 });
