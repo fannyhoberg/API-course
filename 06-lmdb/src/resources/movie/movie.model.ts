eec4da1a363617e329e549b503e3bf4ac15ab3ab
Fanny Hoberg <fannyh.92@hotmail.com>
2024-02-14 10:37:39 +0100

    added more validation rules in movie.model

--- 06-lmdb/src/resources/movie/movie.model.ts
+++ 06-lmdb/src/resources/movie/movie.model.ts
@@ -4,22 +4,49 @@ import { model, Schema, Document } from "mongoose";
 // this is for developers, so we can use Typscript
 export interface MovieDocument extends Document {
   title: string;
-  runtime?: number;
+  runtime: number | null;
   release_year?: number;
+  genres: string[];
+  watched: Date;
 }
 
 // Schema for mongoose, same as interface but we have to use mongoose structure
 // if we need to add keys to schema we have to update interface also
 const MovieSchema: Schema = new Schema<MovieDocument>({
-  title: { type: String, required: true },
+  title: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 3,
+  },
   runtime: {
     type: Number,
-    min: 1,
+    // min: 1,
+    default: null,
+    validate(value: number) {
+      if (value < 1 && value !== null) {
+        throw new Error(
+          "Just because you thought the movie was bad, it shouldn't have zero or negative runtime"
+        );
+      }
+    },
   },
   release_year: {
     type: Number,
     min: 1895,
     max: new Date().getFullYear(),
+  },
+  genres: {
+    type: [String],
+    lowercase: true,
+    default: [],
+    // enums: ["action", "sci-fi", "romance", "realism"]
+  },
+  watched: {
+    type: Date,
+    default() {
+      return Date.now();
+    },
   },
 });
 
