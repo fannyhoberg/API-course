a4a7e6b78b032db66bec16946f8b3d6cc09c5b50
Fanny Hoberg <fannyh.92@hotmail.com>
2024-02-21 10:46:48 +0100

    added function to get messages to display on DOM, also with class own-message to add users messages to the right and friends to the left

--- 07-chat/frontend/src/main.ts
+++ 07-chat/frontend/src/main.ts
@@ -1,8 +1,8 @@
 import { io, Socket } from "socket.io-client";
 import {
-	ChatMessageData,
-	ClientToServerEvents,
-	ServerToClientEvents
+  ChatMessageData,
+  ClientToServerEvents,
+  ServerToClientEvents,
 } from "@shared/types/SocketTypes";
 import "./assets/scss/style.scss";
 
@@ -10,50 +10,92 @@ const SOCKET_HOST = import.meta.env.VITE_SOCKET_HOST;
 console.log("SOCKET_HOST:", SOCKET_HOST);
 
 const messageEl = document.querySelector("#message") as HTMLInputElement;
-const messageFormEl = document.querySelector("#message-form") as HTMLFormElement;
+const messageFormEl = document.querySelector(
+  "#message-form"
+) as HTMLFormElement;
 const messagesEl = document.querySelector("#messages") as HTMLDivElement;
 
 // Connect to Socket.IO Server
-const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(SOCKET_HOST);
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+  io(SOCKET_HOST);
+
+const addMessageToChat = (msg: ChatMessageData, ownMessage = false) => {
+  const msgEl = document.createElement("li");
+
+  // Set class of LI to "message"
+  msgEl.classList.add("message");
+
+  // If the message is from the user add class "own-message"
+  if (ownMessage) {
+    msgEl.classList.add("own-message");
+  }
+
+  // Set text content of the LI element to the message
+  msgEl.textContent = msg.content;
+
+  // Append the LI element to the messages element
+  messagesEl.appendChild(msgEl);
+};
 
 // Listen for when connection is established
 socket.on("connect", () => {
-	console.log("💥 Connected to the server", socket.id);
+  console.log("💥 Connected to the server", socket.id);
 });
 
 // Listen for when server got tired of us
 socket.on("disconnect", () => {
-	console.log("💀 Disconnected from the server");
+  console.log("💀 Disconnected from the server");
 });
 
 // Listen for when the nice server says hello
 socket.on("hello", () => {
-	console.log("🤩 Hello! Is it me you're looking for?");
+  console.log("🤩 Hello! Is it me you're looking for?");
+});
+
+// Listen for new chat messages
+socket.on("chatMessage", (msg) => {
+  console.log("📨 YAY SOMEONE WROTE SOMETHING!!!!!!!", msg);
+
+  /**
+   * @todo 1
+   * Create a function `addMessageToChat` that takes the
+   * `msg` object as a parameter and creates a new LI-element,
+   * sets the content + styling and appends it to `messagesEl`
+   */
+  addMessageToChat(msg);
 });
 
 // Send a message to the server when form is submitted
 messageFormEl.addEventListener("submit", (e) => {
-	e.preventDefault();
+  e.preventDefault();
 
-	// 💇
-	const trimmedMessage = messageEl.value.trim();
+  // 💇
+  const trimmedMessage = messageEl.value.trim();
 
-	// If no message, no send
-	if (!trimmedMessage) {
-		return;
-	}
+  // If no message, no send
+  if (!trimmedMessage) {
+    return;
+  }
 
-	// Construct message payload
-	const msg: ChatMessageData = {
-		content: trimmedMessage,
-	}
+  // Construct message payload
+  const msg: ChatMessageData = {
+    content: trimmedMessage,
+  };
 
-	// Send (emit) the message to the server
-	socket.emit("sendChatMessage", msg);
+  // Send (emit) the message to the server
+  socket.emit("sendChatMessage", msg);
 
-	console.log("Emitted 'sendChatMessage' event to server", msg);
+  console.log("Emitted 'sendChatMessage' event to server", msg);
 
-	// Clear the input field
-	messageEl.value = "";
-	messageEl.focus();
+  /**
+   * @todo 2
+   * Extend the `addMessageToChat` function to know if the msg
+   * was sent by us, and if so add `.own-message` class to the
+   * LI-element before appending it to `messagesEl`
+   */
+  addMessageToChat(msg, true);
+
+  // Clear the input field
+  messageEl.value = "";
+  messageEl.focus();
 });
